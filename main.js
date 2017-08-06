57d5bb7c9a7db5d573643fef17da5d72a17a837f
Candace Moorer <candypattie@me.com>
2017-08-06 17:56:59 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,49 @@
+// Since the `data.js` file is loaded in your `index.html` before this one, 
+// you have access to the `data` variable that contains the Etsy Store data.
+// Open your `index.html` page in your browser and you can verify that the following
+// is properly working. The `data` variable is an array with 25 items in it
+console.log(data);
+
+
+
+// 1: Show me how to calculate the average price of all items.
+function question1 () {
+  let listitems = document.getElementsByTagName( "price");
+  element.classList.add("price");
+
+}
+
+
+
+// 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
+function question2 () {
+  let listitems = document.getElementsByTagName( "price");
+  let element = document.querySelector("$14.00","$15.00","$16.00","$17.00","$18.00");
+}
+
+
+
+// 3: Which item has a "GBP" currency code? Display it's name and price.
+function question3 () {
+  document.getElementById("currency code");
+}
+
+
+// 4: Display a list of all items who are made of wood.
+function question4 () {
+  element.querySelectorAll("wood");
+}
+
+
+// 5: Which items are made of eight or more materials? 
+//    Display the name, number of items and the items it is made of.
+function question5 () {
+  let element = document.getElementById( "title","materials");
+}
+
+
+// 6: How many items were made by their sellers?
+// Answer:
+function question6 () {
+  let element = document.getElementById( "who_made");
+}
